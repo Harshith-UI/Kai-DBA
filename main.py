57502db4803c9fac26dfc82fe5100d5b73fb44f4
Harshith-UI <harshikapuluru@gmail.com>
2026-05-24 18:30:31 +0530

    Hid sensitive URLs from source code

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from oracle_retrieve import retrieve_oracle
 from oracle_chain import get_oracle_answer
-import requests
-
+import requests,os
+from dotenv import load_dotenv
+load_dotenv()
 if __name__ == "__main__":
     question = "Customer ACME account 883921 failing in PRODDB01. ORA-00001 unique constraint violated."
-    url="https://n8n.manifestingpodcasts.site/webhook/sanitize"
+    url=os.getenv("REDACT_URL")
     data={"question":question}
     retrieved_data = retrieve_oracle(question)
     if retrieved_data["found"]:
